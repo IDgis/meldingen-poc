772c6d84c10995d2b8d6ee17c8f42082c4f659d4
Sandro Neumann <sandro.neumann@idgis.nl>
2017-03-09 17:48:51 +0100

    fixed base layer: tiles locally stored

--- imports/ui/message/message.js
+++ imports/ui/message/message.js
@@ -18,56 +18,22 @@ const iconStyle = new ol.style.Style({
 
 Template.message.onRendered(function () {
 	var projection = new ol.proj.Projection({
-		code: 'EPSG:28992',
-		extent: [170000, 300000, 213000, 412000]
+		code: 'EPSG:3857',
+		extent: [-20026376.39, -20048966.10, 20026376.39, 20048966.10]
 	});
 
 	var view = new ol.View({
 		projection: projection,
-		center: [191500, 356000],
-		zoom: 2
+		center: [638402.060238, 6729515.970227],
+		zoom: 8
 	});
 
 	var zoomControl = new ol.control.Zoom();
 	
-	var resolutions = 
-		[3440.64, 1720.32, 860.16, 430.08, 215.04, 107.52, 53.76, 26.88, 13.44, 6.72, 3.36, 1.68, 0.84, 0.42, 0.21];
-	var matrixIds0 = $.map(resolutions, function(resolution) {
-		var indexResolution = resolutions.indexOf(resolution);
-		return 'EPSG:28992' + ':' + indexResolution;
-	});
-	
-	var tileExtent = [-285401.920000, 22598.080000, 595401.920000, 903401.920000];
-	var tileOrigin = [-285401.920000, 22598.080000]
-	
-	var tileGrid0 = new ol.tilegrid.WMTS({
-		origin: tileOrigin,
-		resolutions: resolutions,
-		matrixIds: matrixIds0
-	});
-
 	var achtergrond = new ol.layer.Tile({
-		preload: 1,
-		source: new ol.source.TileImage({
-			crossOrigin: null,
-			extent: tileExtent,
-			projection: new ol.proj.Projection({
-				code: 'EPSG:28992',
-				extent: tileExtent
-			}),
-			tileGrid: tileGrid0,
-			tileUrlFunction: function(coordinate) {
-				if(coordinate === null) {
-					return undefined;
-				}
-
-                var z = coordinate[0];
-                var x = coordinate[1];
-                var y = coordinate[2];
-                var url = 'maps/' + z + '/' + x + '/' + y + '.png';
-                return url;
-            }
-        })
+	    source: new ol.source.OSM({
+	    	url: 'maps/{z}/{x}/{y}.png'
+	    })
 	});
 	
 	var afdelingen = new ol.layer.Vector({
