5c40650d8f0264ec8333837a6445521653740663
Mart Nijzink <mart.nijzink@idgis.nl>
2017-03-15 14:50:51 +0100

    move subscription

--- imports/startup/client/startup.js
+++ imports/startup/client/startup.js
@@ -1,7 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(function() {
-	Meteor.subscribe('message');
-	Meteor.subscribe('afdelingen');
-	Meteor.subscribe('contracten');
+	
 });
--- imports/ui/map/map.js
+++ imports/ui/map/map.js
@@ -26,58 +26,55 @@ Template.mapTemplate.onRendered(function () {
 	
 	geoJsonLayer.addTo(map);
 
-	var initializing = true;
-	Afdelingen.find().observeChanges({
-		added: function (id, fields) {
-			if (!initializing) {
-				map.spin(true);
-				updateLayer(geoJsonLayer, Afdelingen.find());
-				map.spin(false);
-			}
-		},
-		changed: function (id, fields) {
-			if (!initializing) {
-				map.spin(true);
-				updateLayer(geoJsonLayer, Afdelingen.find());
-				map.spin(false);
-			}
-		},
-		removed: function (id) {
-			if (!initializing) {
-				map.spin(true);
-				updateLayer(geoJsonLayer, Afdelingen.find());
-				map.spin(false);
-			}
-		}
-	});
-	initializing = false;
+	var self = this;
 	
-	Message.find().observeChanges({
-		added: function (id, fields) {
-			addMarker(markers, fields.coordinates, fields.description, map);
-		},
-		removed: function (id) {
-			removeMarker('someID');
-		}
+	self.autorun(function() {
+		if (Meteor.status().connected) {
+			var initialized = false;
+			
+			Meteor.subscribe('afdelingen', function() {
+				console.log('afdelingen subscription finished');
+				
+				updateLayer(geoJsonLayer, Afdelingen.find());
+				/*
+				Afdelingen.find().observe({
+					added: function (doc) {
+						if (initialized) {
+							updateLayer(geoJsonLayer, Afdelingen.find());
+						}
+					},
+					changed: function (doc) {
+						if (initialized) {
+							updateLayer(geoJsonLayer, Afdelingen.find());
+						}
+					},
+					removed: function (doc) {
+						if (initialized) {
+							updateLayer(geoJsonLayer, Afdelingen.find());
+						}
+					}
+				});
+				*/
+			});
+			
+			initialized = true;
+			
+			Meteor.subscribe('message', function() {
+				console.log('message subscription finished');
+				
+				Message.find().observeChanges({
+					added: function (id, fields) {
+						addMarker(markers, fields.coordinates, fields.description, map);
+					},
+					removed: function (id) {
+						removeMarker('someID');
+					}
+				});
+			});
+			
+			Meteor.subscribe('contracten');
+		};
 	});
-	
-	/*
-	Tracker.autorun(function () {
-		console.log('collection changed, clearing layer');
-		
-		geoJsonLayer.clearLayers();
-		
-		geoJsonLayer.eachLayer(function (layer) {
-			console.log('not cleared');
-		});
-		
-		console.log('adding ' + Afdelingen.find().count() + ' features');
-
-		Afdelingen.find().forEach(function (feature) {
-			geoJsonLayer.addData(feature);
-		});
-	});
-	*/
 });
 
 function updateLayer(geoJsonLayer, features) {
@@ -93,7 +90,27 @@ function updateLayer(geoJsonLayer, features) {
 }
 
 function addMarker(markers, coordinates, info, map) {
-	var marker = new L.marker(coordinates[0], coordinates[1]);
+	console.log('adding marker at [' + coordinates[0] + ', ' + coordinates[1] + ']');
+	
+	var defaultIcon = L.icon({
+		iconUrl: 'images/location.svg',
+		iconSize:     [32, 32], // size of the icon
+		iconAnchor:   [16, 32], // point of the icon which will correspond to marker's location
+		popupAnchor:  [0, -4] // point from which the popup should open relative to the iconAnchor
+	});
+	
+	// const iconStyle = new ol.style.Style({
+		// image: new ol.style.Icon(({
+			// anchor: [0.5, 32],
+			// anchorXUnits: 'fraction',
+			// anchorYUnits: 'pixels',
+			// opacity: 0.75,
+			// src: 'images/location.svg',
+			// size: [32, 32]
+		// }));
+	// });
+	
+	var marker = new L.marker([coordinates[0], coordinates[1]], {icon: defaultIcon});
 	marker.bindPopup(info);
 	markers.push(marker);
 	marker.addTo(map);
@@ -106,6 +123,7 @@ function removeMarker(id) {
 
 function onMapClick(e) {
 	const latlng = e.latlng;
+	console.log('map clicked at [' + latlng.lat + ', ' + latlng.lng + ']');
 	$('#message-input-modal').modal();
 	$('#js-form-message-coordinates').attr('value', latlng.lat + ',' + latlng.lng);
 }
@@ -134,6 +152,7 @@ Template.mapTemplate.helpers({
 AutoForm.addHooks('messageform', {
 	onSubmit: function (insertDoc, updateDoc, currentDoc) {
 			Message.insert(insertDoc);
+			console.log('inserting doc');
 			this.done();
 			return false;
 		},
